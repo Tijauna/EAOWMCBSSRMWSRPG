f6221fd4128fa0906417bf08865d3ee47b288ac7
Tijauna <daniel.ding@mail.utoronto.ca>
2023-04-10 21:45:15 -0400

    Single handedly program the game

---
+++ game.py
@@ -0,0 +1,10 @@
+print("This is an amazing game wow!")
+
+dingBank = 0
+print(dingBank, "is in Ding's bank account before this game.")
+
+gameProfits = 1999999
+
+dingBank += gameProfits
+
+print(dingBank, " is in Ding's bank account after this game.")
